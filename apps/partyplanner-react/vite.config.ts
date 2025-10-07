1d392402446beb147357944f9777d62c9f69655e
Eric Polman <ericpolman900@gmail.com>
2025-10-07 11:33:13 +0200

    updated vite config

--- apps/partyplanner-react/vite.config.ts
+++ apps/partyplanner-react/vite.config.ts
@@ -3,17 +3,17 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from './vite-plugins/netlify.ts'
+import createNetlifyTanstackStartPlugin from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
-    netlify,
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    createNetlifyTanstackStartPlugin(),
     viteReact(),
   ],
 })
